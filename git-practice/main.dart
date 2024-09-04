9ad64774e78ce5628cda03182ae6042dca9ca967
Meles-Z <meles.zawdie@gmail.com>
2024-09-04 09:29:59 +0300

    added new image

---
+++ git-practice/main.dart
@@ -0,0 +1,3 @@
+void main() {
+  print("Testing mergin with something else");
+}
--- git-practice/test-git.dart
+++ git-practice/test-git.dart
@@ -1,3 +1,18 @@
 void main() {
-  print("Hello there lets test our git experience");
+  print("In through my learning path I have learned the git experience below");
+  /*
+ The difference between git add . and git add --all 
+ both of them can track the file and make it ready for commit but the difference between them are 
+ git add . is trackin file on current directory and subdirectory of the current directory while git add --all is 
+ tracking all directories including root directory that means it trackes all the file and folder inside root directory
+
+ git log --uded for to view of the commit history for repository
+
+ 
+$ git log
+
+git checkout -b branch-name to create git branch and checkout to new branch created
+
+
+ */
 }
