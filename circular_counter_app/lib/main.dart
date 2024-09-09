46cb4651e1d8cb8d588105cc3d0939cfc58deab3
Meles-Z <meles.zawdie@gmail.com>
2024-09-09 21:07:15 +0300

    all change

--- circular_counter_app/lib/main.dart
+++ circular_counter_app/lib/main.dart
@@ -1,24 +1,14 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
 
-sealed class CounterEvent {}
-
-final class CounterIncrementPressed extends CounterEvent {}
-
-class CounterBlock extends Bloc<CounterEvent, int> {
-  CounterBlock() : super(0) {
-    on<CounterIncrementPressed>((event, emit) {
-      emit(state + 1);
-    });
-  }
+void main() {
+  runApp(const MyApp());
 }
 
-Future<void> main() async {
-  final block = CounterBlock();
-  print(block.state);
-  block.add(CounterIncrementPressed());
-  final subscription = block.stream.listen(print);
-  await Future.delayed(Duration.zero);
-  print(block.state);
-  await subscription.cancel();
-  await block.close();
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
 }
